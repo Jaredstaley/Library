1fb6383c56d0562d04371c76834ceea9acd2352a
Jaredstaley <jared.staley242@gmail.com>
2025-11-19 14:11:09 +0000

    Add Java project files

---
+++ Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private String title;
+    private String author;
+    private String bookID;
+
+
+    public Book(String title, String author, String bookID){
+        this.title = title;
+        this.author = author;
+        this.bookID = bookID;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getBookID(){
+        return bookID;
+    }
+
+    public String toString(){
+        return title + " Is authored by " + author;
+    }
+
+}
