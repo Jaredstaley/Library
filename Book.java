774055dcbd99fd6df370a61d9594eed6d47f54d0
Jaredstaley <jared.staley242@gmail.com>
2025-11-19 14:40:28 +0000

    Added getter and setter for Available in Book.java

--- Book.java
+++ Book.java
@@ -2,12 +2,14 @@ public class Book {
     private String title;
     private String author;
     private String bookID;
+    private Boolean available;
 
 
     public Book(String title, String author, String bookID){
         this.title = title;
         this.author = author;
         this.bookID = bookID;
+        this.available = true;
     }
 
     public String getTitle(){
@@ -22,8 +24,16 @@ public class Book {
         return bookID;
     }
 
+    public boolean getAvailable(){
+        return available;
+    }
+
+    public void setAvailable(){
+        available = !available;
+    }
+
     public String toString(){
         return title + " Is authored by " + author;
     }
-
+    
 }
